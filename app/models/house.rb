6781f00e6db539d50668db2c0bb8649d706ebbee
Kevin Cornea <kpcornea@gmail.com>
2018-12-26 20:58:02 -0800

    Add validations to Person model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,12 @@
+# Schema Information
+
+# Table name: houses
+
+# id
+# t.string "address", null: false
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+
+
 class House < ApplicationRecord
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,14 @@
+# Schema Information
+
+# Table name: people
+
+# id
+# t.string "name", null: false
+# t.integer "house_id", null: false
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+
+
 class Person < ApplicationRecord
+  validates :name, :house_id, presence: true
 end
